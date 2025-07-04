a2b65e6af86418d0195f51b326e469561fd33883
Hikabu <eatingvulfrik@gmail.com>
2025-07-04 17:52:43 +0700

    Visibility & Mutability

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -1,20 +1,30 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.30; // grater than 30 
-// @custom:dev-run-script NatSpec
-// contract SimpleStorage {
-//     bool isLong;
-//     uint stringNumber = 42;
-//     int limitOfMsg = 20;
-//     address metaMask = 0x0790248b39886759cA52dfCf44801E5AC0414c4f;
 
-// }
+contract SimpleStorage {
+    address metaMask = 0x0790248b39886759cA52dfCf44801E5AC0414c4f;
+    uint256 public nbr;
+    string  public newStr = "im string";
+    bytes8 public lala = "working";
 
-contract SimpleStorage{
-    function getBytes() public pure returns (bytes memory, bytes8, bytes32){
-        bytes memory dynamicBytes = bytes("cat");
-        bytes8 fixedBytes8 = "cat";
-        // bytes2 fixedBytes2 = "cat";
-        bytes32 fixedBytes32 = "cat";
-        return (dynamicBytes, fixedBytes8, fixedBytes32);
+//updare nbr with input nbr
+    function updateNbr(uint256 newNbr) public {
+        nbr = newNbr;
+    }
+//to see what bytes will return
+// pure function can not read from state or env and can not write
+    function getBytes() public pure returns (bytes memory, bytes8, bytes32, string memory){
+        bytes memory autoCntBytes = bytes("bytes");
+        bytes8 fixedBytes8 = "bytes";
+        bytes32 fixedBytes32 = "bytes";
+        // string memory notWorks = newStr; //not working needs to be view
+        string memory str = "bytes";
+        return (autoCntBytes, fixedBytes8, fixedBytes32, str);
+    }
+//updating with global var i can access state bcs view ,method 
+    function something() public view returns (bytes8){
+         bytes8 fixedBytes8 = lala;
+        //  bytes32 fix32 = newStr;//diff types
+         return (fixedBytes8);
     }
 }
